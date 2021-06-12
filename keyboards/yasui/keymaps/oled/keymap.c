cc665aa29e7dc688a9a7bb25b459a3d8d81af7ac
Robert Baxter <rlbaxter.us@gmail.com>
2021-06-12 14:46:39 -0600

    yasui layout

--- keyboards/yasui/keymaps/oled/keymap.c
+++ keyboards/yasui/keymaps/oled/keymap.c
@@ -15,33 +15,58 @@
  */
 #include "yasui.h"
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-	KEYMAP(
-		KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
-		KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_ENT,
-		KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_BSPC,
-		KC_LCTL, KC_LALT, MO(1), KC_LSFT, KC_MPLY, KC_SPC, KC_SPC, MO(2), KC_NO, KC_NO),
-
-	KEYMAP(
-		KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0,
-		KC_ESC, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_MINS, KC_EQL, KC_QUOT, KC_SCLN, KC_TRNS,
-		KC_TAB, KC_DEL, KC_PSCR, KC_LBRC, KC_RBRC, KC_BSLS, KC_SLSH, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(4), KC_TRNS, KC_TRNS),
-
-	KEYMAP(
-		KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-		KC_GRV, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UNDS, KC_PLUS, KC_DQUO, KC_COLN, KC_TRNS,
-		KC_TRNS, KC_TRNS, TG(3), KC_LCBR, KC_RCBR, KC_PIPE, KC_QUES, KC_LABK, KC_RABK, KC_TRNS,
-		KC_TRNS, KC_TRNS, MO(4), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-
-	KEYMAP(
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TG(3), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
-
+enum layers_names {
+  _QWERTY,
+  _LOWER,
+  _RAISE,
+  _ADJUST,
+  _EXTRAS
 };
+
+#define LOWER MO(_LOWER)
+#define RAISE MO(_RAISE)
+#define EXTRAS LT(_EXTRAS, KC_TAB)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+[_QWERTY] = KEYMAP(
+  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,   KC_I,     KC_O,   KC_P,
+  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,   KC_K,     KC_L,   KC_BSPC,
+  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,   KC_COMM,  KC_DOT, KC_ENT,
+  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_LSFT, KC_SPC,  RAISE,  EXTRAS,   XXXXXXX, XXXXXXX
+),
+
+[_LOWER] = KEYMAP(
+  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
+  XXXXXXX, KC_MPLY, KC_PLUS, KC_UNDS, KC_QUES, KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, _______,
+  XXXXXXX, KC_MPRV, KC_MNXT, KC_PIPE, KC_TILD, KC_DQUO, KC_COLN, KC_LCBR, KC_RCBR, KC_ESC,
+  _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX
+),
+
+[_RAISE] = KEYMAP(
+  KC_1,    KC_2,    KC_3,    KC_4,     KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+  XXXXXXX, KC_MUTE, KC_EQL,  KC_MINS,  KC_SLSH, KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, _______,
+  XXXXXXX, KC_VOLD, KC_VOLU, KC_BSLS,  KC_GRV,  KC_QUOT, KC_SCLN, KC_LBRC, KC_RBRC, KC_ESC,
+  _______, _______, _______, _______,  _______, _______, _______, _______, XXXXXXX, XXXXXXX
+),
+
+[_ADJUST] = KEYMAP(
+  RESET,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_DEL,
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F11,  KC_F12,
+  _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX
+),
+
+[_EXTRAS] = KEYMAP(
+  KC_PPLS, KC_P7,   KC_P8,   KC_P9,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  KC_PMNS, KC_P4,   KC_P5,   KC_P6,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  KC_PAST, KC_P1,   KC_P2,   KC_P3,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, KC_P0,   KC_PDOT, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSLS, _______, XXXXXXX, XXXXXXX
+)
+};
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
 
 void encoder_update_user(uint8_t index, bool clockwise) {
 	switch (get_highest_layer(layer_state)) {
@@ -84,21 +109,24 @@ void oled_task_user(void) {
     sprintf(wpm, "WPM: %03d", get_current_wpm());
 
     // Host Keyboard Layer Status
-    oled_write_P(PSTR("       rainkeeb \n"), false);
+    oled_write_P(PSTR("       yasui \n"), false);
     oled_write_P(PSTR(" Layer: "), false);
 
     switch (get_highest_layer(layer_state)) {
-        case 0:
+        case _QWERTY:
             oled_write_P(PSTR("Default\n"), false);
             break;
-        case 1:
+        case _RAISE:
             oled_write_P(PSTR("Raise\n"), false);
             break;
-        case 2:
+        case _LOWER:
+            oled_write_P(PSTR("Lower\n"), false);
+            break;
+        case _ADJUST:
             oled_write_P(PSTR("Adjust\n"), false);
             break;
-        case 3:
-            oled_write_P(PSTR("Game\n"), false);
+        case _EXTRAS:
+            oled_write_P(PSTR("Extras\n"), false);
             break;
         default:
             // Or use the write_ln shortcut over adding '\n' to the end of your string
