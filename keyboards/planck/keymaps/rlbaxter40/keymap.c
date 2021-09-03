3cbcf79ed708fc3c9cd4dad0594852849d2bb751
Robert Baxter <rlbaxter.us@gmail.com>
2021-09-02 22:07:28 -0600

    update 10 planck to use 2u space bars

--- keyboards/planck/keymaps/rlbaxter40/keymap.c
+++ keyboards/planck/keymaps/rlbaxter40/keymap.c
@@ -39,28 +39,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX, XXXXXXX, KC_Y,    KC_U,   KC_I,     KC_O,   KC_P,
   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    XXXXXXX, XXXXXXX, KC_H,    KC_J,   KC_K,     KC_L,   KC_BSPC,
   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    XXXXXXX, XXXXXXX, KC_N,    KC_M,   KC_COMM,  KC_DOT, KC_ENT,
-  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_LSFT, XXXXXXX, XXXXXXX, KC_SPC,  RAISE,  EXTRAS,   WIN_L,  WIN_R
+  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_LSFT, KC_LSFT, KC_SPC,  KC_SPC,  RAISE,  EXTRAS,   WIN_L,  WIN_R
 ),
 
 [_LOWER] = LAYOUT_planck_grid(
   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, XXXXXXX, XXXXXXX, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
   XXXXXXX, XXXXXXX, KC_PLUS, KC_UNDS, KC_QUES, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, _______,
   XXXXXXX, XXXXXXX, XXXXXXX, KC_PIPE, KC_TILD, XXXXXXX, XXXXXXX, KC_DQUO, KC_COLN, KC_LCBR, KC_RCBR, KC_ESC,
-  _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX, _______, _______, KC_MPLY, KC_MPRV, KC_MNXT
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_MPRV, KC_MNXT
 ),
 
 [_RAISE] = LAYOUT_planck_grid(
   KC_1,    KC_2,    KC_3,    KC_4,     KC_5,    XXXXXXX, XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
   XXXXXXX, XXXXXXX, KC_EQL,  KC_MINS,  KC_SLSH, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, _______,
   XXXXXXX, XXXXXXX, XXXXXXX, KC_BSLS,  KC_GRV,  XXXXXXX, XXXXXXX, KC_QUOT, KC_SCLN, KC_LBRC, KC_RBRC, KC_ESC,
-  _______, _______, _______, _______,  _______, XXXXXXX, XXXXXXX, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU
+  _______, _______, _______, _______,  _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU
 ),
 
 [_ADJUST] = LAYOUT_planck_grid(
   RESET,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, XXXXXXX, XXXXXXX, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_DEL,
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   XXXXXXX, XXXXXXX, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
   MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   XXXXXXX, XXXXXXX, MI_OFF,  AU_ON,   AU_OFF,  KC_F11,  KC_F12,
-  MU_MOD, _______,  _______, _______, _______, XXXXXXX, XXXXXXX, _______, _______, _______, XXXXXXX, XXXXXXX
+  MU_MOD, _______,  _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX
 ),
 
 [_EXTRAS] = LAYOUT_planck_grid(
