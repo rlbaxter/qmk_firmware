a382c9abdf4ccc6970ad8b606342c9f985994c91
Robert Baxter <rlbaxter.us@gmail.com>
2022-07-31 11:36:56 -0600

    add the default ut11 files

---
+++ keyboards/ut11/keymaps/default/keymap.c
@@ -0,0 +1,10 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    LAYOUT_ut11(
+        KC_A,    KC_B,    
+        KC_D,    KC_E,    KC_F,    
+        KC_G,    KC_H,    KC_I,    
+        KC_J,    KC_K,    KC_L     
+    )
+};
