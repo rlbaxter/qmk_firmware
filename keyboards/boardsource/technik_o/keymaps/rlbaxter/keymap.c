cda4aa6cfe51cb16a90a5d68706b84988f4cda62
Robert Baxter <rlbaxter.us@gmail.com>
2024-07-24 18:28:00 -0600

    update my technik layout to canary

--- keyboards/boardsource/technik_o/keymaps/rlbaxter/keymap.c
+++ keyboards/boardsource/technik_o/keymaps/rlbaxter/keymap.c
@@ -16,80 +16,59 @@
 
 #include QMK_KEYBOARD_H
 
-enum planck_layers {
-  _QWERTY,
-  _LOWER,
+enum layers_names {
+  _CANARY,
   _RAISE,
-  _ADJUST,
-  _EXTRAS
+  _LOWER,
+  _ADJUST
 };
 
-#define LCTLS MT(MOD_LCTL, KC_S)
-#define LALTD MT(MOD_LALT, KC_D)
-#define LSFTF MT(MOD_LSFT, KC_F)
+#define LCTLR MT(MOD_LCTL, KC_R)
+#define LALTS MT(MOD_LALT, KC_S)
+#define LSFTT MT(MOD_LSFT, KC_T)
 #define LGUIG MT(MOD_LGUI, KC_G)
-#define RGUIH MT(MOD_RGUI, KC_H)
-#define RSFTJ MT(MOD_RSFT, KC_J)
-#define RALTK MT(MOD_LALT, KC_K)
-#define RCTLL MT(MOD_LCTL, KC_L)
+#define RGUIM MT(MOD_RGUI, KC_M)
+#define RSFTN MT(MOD_RSFT, KC_N)
+#define RALTE MT(MOD_LALT, KC_E)
+#define RCTLI MT(MOD_LCTL, KC_I)
+
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
-#define EXTRAS LT(_EXTRAS, KC_TAB)
 #define WIN_L C(KC_LEFT)
 #define WIN_R C(KC_RIGHT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[_QWERTY] = LAYOUT_ortho_4x12(
-  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX, XXXXXXX, KC_Y,    KC_U,   KC_I,     KC_O,   KC_P,
-  KC_A,    LCTLS,   LALTD,   LSFTF,   LGUIG,   XXXXXXX, XXXXXXX, RGUIH,   RSFTJ,  RALTK,    RCTLL,  KC_BSPC,
-  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    XXXXXXX, XXXXXXX, KC_N,    KC_M,   KC_COMM,  KC_DOT, KC_ENT,
-  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_LSFT, XXXXXXX, XXXXXXX, KC_SPC,  RAISE,  EXTRAS,   WIN_L,  WIN_R
-),
-
-[_LOWER] = LAYOUT_ortho_4x12(
-  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, XXXXXXX, XXXXXXX, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-  XXXXXXX, XXXXXXX, KC_PLUS, KC_UNDS, KC_QUES, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, _______,
-  XXXXXXX, XXXXXXX, XXXXXXX, KC_PIPE, KC_TILD, XXXXXXX, XXXXXXX, KC_DQUO, KC_COLN, KC_LCBR, KC_RCBR, KC_ESC,
-  _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX, _______, _______, KC_MPLY, KC_MPRV, KC_MNXT
+[_CANARY] = LAYOUT_ortho_4x12(
+  KC_W,    KC_L,    KC_Y,    KC_P,    KC_B,    XXXXXXX, XXXXXXX, KC_Z,    KC_F,   KC_O,     KC_U,   KC_BSPC,
+  KC_C,    LCTLR,   LALTS,   LSFTT,   LGUIG,   XXXXXXX, XXXXXXX, RGUIM,   RSFTN,  RALTE,    RCTLI,  KC_A,
+  KC_Q,    KC_J,    KC_V,    KC_D,    KC_K,    XXXXXXX, XXXXXXX, KC_X,    KC_H,   KC_COMM,  KC_DOT, KC_ENT,
+  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_LSFT, XXXXXXX, XXXXXXX, KC_SPC,  RAISE,  KC_TAB,   WIN_L,  WIN_R
 ),
 
 [_RAISE] = LAYOUT_ortho_4x12(
   KC_1,    KC_2,    KC_3,    KC_4,     KC_5,    XXXXXXX, XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-  XXXXXXX, XXXXXXX, KC_EQL,  KC_MINS,  KC_SLSH, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, _______,
-  XXXXXXX, XXXXXXX, XXXXXXX, KC_BSLS,  KC_GRV,  XXXXXXX, XXXXXXX, KC_QUOT, KC_SCLN, KC_LBRC, KC_RBRC, KC_ESC,
-  _______, _______, _______, _______,  _______, XXXXXXX, XXXXXXX, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU
+  XXXXXXX, XXXXXXX, KC_EQL,  KC_MINS,  KC_SLSH, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, KC_DEL,
+  KC_MUTE, KC_VOLD, KC_VOLU, KC_BSLS,  KC_GRV,  XXXXXXX, XXXXXXX, KC_QUOT, KC_SCLN, KC_LBRC, KC_RBRC, KC_ESC,
+  _______, _______, _______, _______,  _______, XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______
+),
+
+[_LOWER] = LAYOUT_ortho_4x12(
+  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, XXXXXXX, XXXXXXX, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
+  XXXXXXX, XXXXXXX, KC_PLUS, KC_UNDS, KC_QUES, XXXXXXX, XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_PSCR,
+  KC_MPLY, KC_MPRV, KC_MNXT, KC_PIPE, KC_TILD, XXXXXXX, XXXXXXX, KC_DQUO, KC_COLN, KC_LCBR, KC_RCBR, KC_ESC,
+  _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______
 ),
 
 [_ADJUST] = LAYOUT_ortho_4x12(
-  RESET,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, XXXXXXX, XXXXXXX, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_DEL,
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   XXXXXXX, XXXXXXX, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-  MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   XXXXXXX, XXXXXXX, MI_OFF,  AU_ON,   AU_OFF,  KC_F11,  KC_F12,
-  MU_MOD, _______,  _______, _______, _______, XXXXXXX, XXXXXXX, _______, _______, _______, XXXXXXX, XXXXXXX
-),
-
-[_EXTRAS] = LAYOUT_ortho_4x12(
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PPLS, KC_P7,   KC_P8,   KC_P9,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PMNS, KC_P4,   KC_P5,   KC_P6,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PAST, KC_P1,   KC_P2,   KC_P3,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSLS, _______, KC_P0,   KC_PDOT
+  KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,
+  XXXXXXX, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, XXXXXXX, XXXXXXX, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, XXXXXXX,
+  _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX, _______, _______, _______, XXXXXXX, XXXXXXX
 )
 
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-}
-
-void dip_switch_update_user(uint8_t index, bool active) {
-    switch (index) {
-        case 0: {
-            if (active) {
-                layer_on(_ADJUST);
-            } else {
-                layer_off(_ADJUST);
-            }
-            break;
-        }
-    }
 }
